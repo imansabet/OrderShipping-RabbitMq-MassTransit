87764f5a9b68396fd9b3d25222443bdff5c90b41
iman <dev.imansabet@gmail.com>
2025-07-05 23:07:05 +0330

    Add project files.

---
+++ SharedMessages/Messages/OrderPlaced.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedMessages.Messages;
+
+public sealed record  OrderPlaced(Guid OrderId , int Quantity);
---
+++ TrackingService/Consumers/OrderPlacedConsumer.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using SharedMessages.Messages;
+
+namespace TrackingSwervice.Consumers;
+
+public class OrderPlacedConsumer : IConsumer<OrderPlaced>
+{
+    public Task Consume(ConsumeContext<OrderPlaced> context)
+    {
+        Console.WriteLine($"Order Received For Shipping - {context.Message.OrderId} - quantity {context.Message.Quantity} ");
+
+        return Task.CompletedTask;
+
+    }
+}
